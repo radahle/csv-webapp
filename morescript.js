0d3bf4eeef99bb7a9645b28e1feadb9135e33a64
Rudi A Dahle <dahle05@gmail.com>
2018-09-27 16:04:50 +0200

    url igjen

--- morescript.js
+++ morescript.js
@@ -41,7 +41,7 @@ function readURL(url){
   body.append('br');
 
   // Variables
-  var body = d3.select('body');
+  // var body = d3.select('body');
   var margin = { top: 50, right: 50, bottom: 50, left: 50 };
   var h = 600 - margin.top - margin.bottom;
   var w = 600 - margin.left - margin.right;
