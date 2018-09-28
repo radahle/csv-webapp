70164ffe05c1f566a3419e285873acef9ab8c68f
Lasse.Klovstad <lasse.klovstad@gmail.com>
2018-09-28 12:34:53 +0200

    color grouping and legend

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
 function readFile(file){
     var reader = new FileReader();
     reader.onload = function(event){
-        d3.csv(event.target.result,function (data) {
+        d3.csv(event.target.result,function (data1) {
 // CSV section
 
   var body = d3.select('#scatter');
-  var selectData = d3.keys(data[0]);
+var selectData = [];
+   //var data = data1.filter(filterCriteria);
+  var data = data1;
+  var stringColumn = [];
 
 
-  var newData = data.filter(filterCriteria);
 
-  function filterCriteria(d){
-    return d.species=== "setosa";
+
+
+   
+// check which column is a string and not a number
+   for(var element in data1[1]){
+           if( data[1].hasOwnProperty(element)) {
+               var propValue = data[1][element];
+               if(isNaN(propValue)){
+                  stringColumn.push({name:element,values:[]});
+                                  } else{
+                                    selectData.push(element);
+                                  }
+            }
+  }
+//console.log(stringColumn);
+  for(var element of data1){
+    for(var column of stringColumn){
+      
+      if(column.values.indexOf(element[column.name])==-1){
+        column.values.push(element[column.name]);
+      }
+      
+
+
+    }
+    
   }
 
+  //console.log(stringColumn);
+var dataTest = [];
+  for(var column of stringColumn){
+    for(var value of column.values){
+      var tempData = data1.filter(function(d){
+        return d[column.name]===value;
+      });
+      //console.log(data1);
+      dataTest.push(tempData);
+      
+    }
+  }
+  
+
+  console.log(selectData);
+  
+
+  //var body = d3.select('body');
   // Select X-axis Variable
   var span = body.append('span')
     .text('Select X-Axis variable: ');
@@ -43,7 +87,7 @@ function readFile(file){
   body.append('br');
 
   // Variables
- // var body = d3.select('body');
+  //var body = d3.select('body');
   var margin = { top: 50, right: 50, bottom: 50, left: 50 };
   var h = 600 - margin.top - margin.bottom;
   var w = 600 - margin.left - margin.right;
@@ -53,7 +97,7 @@ function readFile(file){
   var xelement=   d3.select('select#xSelect')[0]['0'].value;
 
 
-        console.log(xelement);
+
 
 
   // Scales
@@ -61,14 +105,14 @@ function readFile(file){
   var xScale = d3.scale.linear()
     .domain([
       d3.min([0,d3.min(data,function (d) { return d[xelement] })]),
-      d3.max([0,d3.max(data,function (d) { return d[xelement] })])
+      d3.max([0,d3.max(data,function (d) { return d[xelement] })*1.3])
       ])
     .range([0,w]);
 
   var yScale = d3.scale.linear()
     .domain([
       d3.min([0,d3.min(data,function (d) { return d[yelement] })]),
-      d3.max([0,d3.max(data,function (d) { return d[yelement] })])
+      d3.max([0,d3.max(data,function (d) { return d[yelement] })*1.3])
       ])
     .range([h,0]);
 
@@ -94,8 +138,10 @@ function readFile(file){
     .orient('left');
 
   // Circles
-  var circles = svg.selectAll('circle')
-      .data(data)
+  var circles = svg.selectAll('circle');
+for(var i in dataTest){
+
+      circles.data(dataTest[i])
       .enter()
     .append('circle')
       .attr('cx',function (d) { return xScale(d[xelement]) })
@@ -103,7 +149,7 @@ function readFile(file){
       .attr('r','10')
       .attr('stroke','black')
       .attr('stroke-width',1)
-      .attr('fill',function (d,i) { return colorScale(i) })
+      .attr('fill',function (d) { return colorScale(i) })
       .on('mouseover', function () {
         d3.select(this)
           .transition()
@@ -118,7 +164,10 @@ function readFile(file){
           .attr('r',10)
           .attr('stroke-width',1)
       })
-    .append('title') // Tooltip
+    .append('title') //
+}
+
+  // Tooltip
      /* .text(function (d) { return d.variable +
                            '\nReturn: ' + formatPercent(d['Annualized Return']) +
                            '\nStd. Dev.: ' + formatPercent(d['Annualized Standard Deviation']) +
@@ -150,12 +199,36 @@ function readFile(file){
       .style('text-anchor','end')
       .text('Y Axis');
 
+
+  // draw legend
+  
+  var legend = svg.selectAll(".legend")
+      .data(stringColumn[0].values)
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  // draw legend colored rectangles
+  legend.append("rect")
+      .attr("x", w + 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", function (d,i) { return colorScale(i) });
+
+  // draw legend text
+  legend.append("text")
+      .attr("x", w -24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d;})
+
   function yChange() {
     var value = this.value; // get the new y value
     yScale // change the yScale
       .domain([
         d3.min([0,d3.min(data,function (d) { return d[value] })]),
-        d3.max([0,d3.max(data,function (d) { return d[value] })])
+        d3.max([0,d3.max(data,function (d) { return d[value]*1.3 })])
         ]);
     yAxis.scale(yScale); // change the yScale
     d3.select('#yAxis') // redraw the yAxis
@@ -174,7 +247,7 @@ function readFile(file){
     xScale // change the xScale
       .domain([
         d3.min([0,d3.min(data,function (d) { return d[value] })]),
-        d3.max([0,d3.max(data,function (d) { return d[value] })])
+        d3.max([0,d3.max(data,function (d) { return d[value]*1.3 })])
         ]);
     xAxis.scale(xScale); // change the xScale
     d3.select('#xAxis') // redraw the xAxis
